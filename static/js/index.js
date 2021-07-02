dea1f0af1d5e2490ddb01727fb2ac996f56313cc
Muhammad Fadhil <33900154+devfdL@users.noreply.github.com>
2021-07-02 16:02:41 +0700

    Add files via upload

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,25 @@
-function uploadFile(target) {
-	document.getElementById("file-name").innerHTML = target.files[0].name;
+function fileinp(){
+	var x = document.getElementById("fileinput");
+	var txt = "";
+	if ('files' in x) {
+	  if (x.files.length == 0) {
+		txt = "Select one or more files.";
+	  } else {
+		for (var i = 0; i < x.files.length; i++) {
+		  var file = x.files[i];
+		  if ('name' in file) {
+			txt += file.name + "<br>";
+		  }
+		}
+	  }
+	} 
+	else {
+	  if (x.value == "") {
+		txt += "Select one or more files.";
+	  } else {
+		txt += "The files property is not supported by your browser!";
+		txt  += "<br>The path of the selected file: " + x.value; // If the browser does not support the files property, it will return the path of the selected file instead. 
+	  }
+	}
+	document.getElementById("demo").innerHTML = txt;
 }
